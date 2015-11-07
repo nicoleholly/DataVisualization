20eb9e88007bcb34c0ff555390495e12ddb9566c
Nataliya Sayenko <nataliyasayenko@gmail.com>
2015-11-07 18:01:21 -0500

    js

--- DiaryData/client/app.js
+++ DiaryData/client/app.js
@@ -16,35 +16,15 @@ Template.form.events({
 Template.visualization.helpers({
 	visualization: function(){	
 		return Transactions.find({userID:Meteor.userId()});
-	},
-
-	colorEmotion: function() {
-		var emotion = $('.currentEmotion').val();
-		console.log(emotion);
-		console.log(this.find("[currentEmotion]"));
-		switch(emotion) {
-			case 'happy':
-				return 'tangerine'
-				break;
-			case 'sad':
-				return 'grey'
-				break;
-			case 'angry':
-				return 'red'
-				break;
-			case 'relaxed':
-				return 'blue'
-				break;
-			case 'fear':
-				return 'black'
-				break;
-			case 'excited':
-				return 'yellow'
-				break;
-			case 'guilt':
-				return 'white'
-				break;
-		}
 	}
+	
 })
 
+Template.visualization.onRendered(function(){
+	console.log('rendered');
+	var emotion = $('.colorEmotion');
+	for (i=0;i<emotion.length; i++){
+		console.log(emotion[i].innerHTML);
+	
+	}
+})
