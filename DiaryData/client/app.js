6a85def86b7683364d2d64e143c51f23cc54662d
Nicole <nhb228@nyu.edu>
2015-11-08 15:53:50 -0500

    changez

--- DiaryData/client/app.js
+++ DiaryData/client/app.js
@@ -38,9 +38,7 @@ function getData(){
 		    			break;
 		    	}
 		    })
-	
-}
-
+};
 
 Template.form.events({
 	"submit form" : function(e) {
@@ -57,33 +55,8 @@ Template.form.events({
 		getData();
 		
 	}
-})
+});
+
 Template.visualization.onRendered(function(){
 	getData();
-})
-
-
-/*
-Template.canvas.onRendered(function(){
-	var scene = new THREE.Scene();
-	var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-	var renderer = new THREE.WebGLRenderer();
-	renderer.setSize( window.innerWidth, window.innerHeight );
-	document.body.appendChild( renderer.domElement );
-
-	var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-	var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-	var cube = new THREE.Mesh( geometry, material );
-	scene.add( cube );
-
-	camera.position.z = 5;
-	function render() {
-		requestAnimationFrame( render );
-		cube.rotation.x += 0.1;
-		cube.rotation.y += 0.1; 
-		renderer.render( scene, camera );
-	}
-	render();
-})
-*/
+});
