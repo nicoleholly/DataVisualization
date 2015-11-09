d87a56c73affed59f2cfed827f73c9437e364140
Nataliya Sayenko <nataliyasayenko@gmail.com>
2015-11-08 21:31:38 -0500

    hey

--- DiaryData/client/app.js
+++ DiaryData/client/app.js
@@ -7,7 +7,7 @@ function getData(){ //to turn this into a method, have to run d3 on server side.
 		    .append("div")
 		    .attr("class", "bar")
 		    .style("height", function(bar) {
-		    	console.log(bar);
+		  
 		        var barHeight = bar.intensity;
 		        return barHeight + "px";
 	    })
@@ -65,27 +65,8 @@ Template.visualization.onRendered(function(){
 
 
 Template.canvas.onRendered(function(){
-	var scene = new THREE.Scene();
-	var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+	 
 
-	var renderer = new THREE.WebGLRenderer();
-	renderer.setSize( window.innerWidth, window.innerHeight);
-	var template = document.getElementById("canvas");
-	template.appendChild( renderer.domElement ); 
-	console.log(renderer.domElement); 
-
-	var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-	var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-	var cube = new THREE.Mesh( geometry, material );
-	scene.add( cube );
-
-	camera.position.z = 5;
-	function render() {
-		requestAnimationFrame( render );
-		cube.rotation.x += 0.1;
-		cube.rotation.y += 0.1; 
-		renderer.render( scene, camera );
-	}
-	render();
 })
+
 
