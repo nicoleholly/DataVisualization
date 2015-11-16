79bb38836ba39ebcaff2c8d26d53cdcd762ea232
Nataliya Sayenko <nataliyasayenko@gmail.com>
2015-11-15 23:29:31 -0500

    reactive

--- DiaryData/client/app.js
+++ DiaryData/client/app.js
@@ -1,85 +1,24 @@
 
 
-function three(dataset){
-	var container, stats;
-	var geometry, group;
-	var mouseX = 0, mouseY = 0;
-	var windowHalfX = window.innerWidth / 2;
-	var windowHalfY = window.innerHeight / 2;
-	document.addEventListener( 'mousemove', onDocumentMouseMove, false );
-
-	 
-
-
 	var scene = new THREE.Scene();
 	var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
 	var renderer = new THREE.WebGLRenderer();
+
 	renderer.setSize( window.innerWidth, window.innerHeight);
 	renderer.setClearColor(0xffffff, 1);
-
-	var template = document.getElementById("canvas");
-	template.appendChild( renderer.domElement ); 
-	console.log(renderer.domElement); 
-
-	var group = new THREE.Group();
-	for ( var i = 0; i < dataset.length; i++) {
-		var size = (dataset[i].intensity/95)
-		var geometry = new THREE.DodecahedronGeometry( 1,0);
-		var material = new THREE.MeshBasicMaterial( { color: switchEmotionColor(dataset[i].emotion),  transparent: true, opacity: 0.5} );
-		var mesh = new THREE.Mesh( geometry, material );
-			mesh.position.x = Math.random() * 10 - 5;
-			mesh.position.y = Math.random() * 10 - 5;
-			mesh.position.z = Math.random() * 10 - 5;
-
-			mesh.rotation.x = Math.random() * 2 * Math.PI;
-			mesh.rotation.y = Math.random() * 2 * Math.PI;
-
-			mesh.matrixAutoUpdate = false;
-			mesh.updateMatrix();
-			group.add( mesh );
-		}
-
-	scene.add( group );
-
 	var cubeMaterial = new THREE.MeshBasicMaterial( { color: 'blue', wireframe: true, transparent: true, opacity: 0.8} );
 	var cubeGeometry = new THREE.DodecahedronGeometry( 1,0);
 	var cube = new THREE.Mesh( cubeGeometry, cubeMaterial );
 	scene.add( cube );
 
-	scene.add( new THREE.AmbientLight( 0x404040 ) );
 
-	camera.position.z = 5;
+function three(){
 
+	var template = document.getElementById("canvas");
+	template.appendChild( renderer.domElement ); 
 
-
-
-	function onDocumentMouseMove( event ) {
-		mouseX = ( event.clientX - windowHalfX ) * .1;
-		mouseY = ( event.clientY - windowHalfY ) * .1;
-	}
-
-	function render() {
-		requestAnimationFrame( render );
-		cube.rotation.x += 0.02;
-		cube.rotation.y += 0.02; 
-		//cube.rotation.z += 0.02;
-
-
-		camera.position.x += ( mouseX - camera.position.x ) * .01;
-		camera.position.y += ( - mouseY - camera.position.y ) * .01;
-
-		camera.lookAt( scene.position );
-		
-		renderer.render( scene, camera );
-	}
-	render();
 	
 }
-
-
-
-
 
 function getData(dataset){ 
     
@@ -160,10 +99,74 @@ Template.visualization.onRendered(function(){
 
 
 Template.canvas.onRendered(function(){
+	console.log("rendered");
+	three();
 
 	this.autorun(function() { 
+	console.log("rerun");
 	var dataset = Template.currentData();
-	three(dataset);
+	var geometry, group;
+	var mouseX = 0, mouseY = 0;
+	var windowHalfX = window.innerWidth / 2;
+	var windowHalfY = window.innerHeight / 2;
+	document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+
+	 
+
+
+		
+		var group = new THREE.Group();
+		console.log(dataset.length);
+	for ( var i = 0; i < dataset.length; i++) {
+		console.log(dataset[i].intensity);
+		var size = (dataset[i].intensity/95);
+		var geometry = new THREE.DodecahedronGeometry( size,0);
+		var material = new THREE.MeshBasicMaterial( { color: switchEmotionColor(dataset[i].emotion),  transparent: true, opacity: 0.5} );
+		var mesh = new THREE.Mesh( geometry, material );
+			mesh.position.x = Math.random() * 10 - 5;
+			mesh.position.y = Math.random() * 10 - 5;
+			mesh.position.z = Math.random() * 10 - 5;
+
+			mesh.rotation.x = Math.random() * 2 * Math.PI;
+			mesh.rotation.y = Math.random() * 2 * Math.PI;
+
+			mesh.matrixAutoUpdate = false;
+			mesh.updateMatrix();
+			group.add( mesh );
+		}
+
+	
+
+	scene.add( group );
+	console.log("WHAT THE FUCK "); //WHAT THE FUCK
+
+
+	camera.position.z = 5;
+
+
+
+
+	function onDocumentMouseMove( event ) {
+		mouseX = ( event.clientX - windowHalfX ) * .1;
+		mouseY = ( event.clientY - windowHalfY ) * .1;
+	}
+
+	function render() {
+		requestAnimationFrame( render );
+		cube.rotation.x = 0.02;
+		cube.rotation.y = 0.02; 
+		//cube.rotation.z += 0.02;
+
+
+		camera.position.x += ( mouseX - camera.position.x ) * .01;
+		camera.position.y += ( - mouseY - camera.position.y ) * .01;
+
+		camera.lookAt( scene.position );
+		
+		renderer.render( scene, camera );
+	}
+	render();
+	
 
 	})
 })
